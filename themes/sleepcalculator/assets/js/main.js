e75e7dbdf45dc7bbe6132ab538c6b537826dd0f2
Frank Spin <info@frankspin.nl>
2019-09-08 17:42:26 +0200

    x

---
+++ themes/sleepcalculator/assets/js/main.js
@@ -0,0 +1,14 @@
+var slider = tns({
+    container: '.slaaptekort ',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controlsPosition: 'bottom',
+    controls: false,
+    nav: true,
+    navPosition: 'bottom',
+    autoplay: true,
+    autoplayButtonOutput: false,
+    loop: true,
+    autoHeight: true
+  });
